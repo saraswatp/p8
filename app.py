07c853b59e936898e89e3c0f7b45f1c56091c47e
saraswatp <48299213+saraswatp@users.noreply.github.com>
2021-02-04 15:03:04 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+from klein import run, route
+import tkinter
+
+app = flask.Flask(__name__)
+
+@route('/')
+def home():
+    
+    window = tkinter.Tk()
+    window.title("Button GUI")
+    button_widget = tkinter.Button(window,text="jai ho")
+    button_widget.pack()
+    tkinter.mainloop()
+run("localhost", 8080)
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
